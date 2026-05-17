15c2bed6f8989d2fda411192fbfc7874bb5a85c3
Samuel Freundel <samuel.freundel@uni-ulm.de>
2026-05-17 15:40:50 +0200

    Revert "Apply bleu"

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
-
-from data.simple_ppdb import SimplePPDB
-
 def main() -> None:
     print("setting up")
-    ppdb: SimplePPDB = SimplePPDB.load_from_disk('data/simple-ppdb/SimplePPDB');    
-    print(ppdb.entries[0])
-    print(f"dimension: {len(ppdb.entries)}")
 
 
 if __name__ == "__main__":
