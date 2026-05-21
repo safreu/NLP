e8cdc8e7a0bde025911cd61399764c4cc83bc590
Samuel Freundel <samuel.freundel@uni-ulm.de>
2026-05-21 19:16:43 +0200

    fix(metrics): fixed bleu score

--- src/metrics/Bleu.py
+++ src/metrics/Bleu.py
@@ -4,7 +4,7 @@ from nltk.translate.bleu_score import corpus_bleu
 def compute_bleuscore(candidates: list[str], references: list[str]) -> float:
     
     candidate_tokens = [candidate.split() for candidate in candidates]
-    reference_tokens = [reference.split() for reference in references]
+    reference_tokens = [[reference.split()] for reference in references]
     
     score = corpus_bleu(
         reference_tokens,
