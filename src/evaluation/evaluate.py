4278caba3752ea949428f3488fc72da6e90c5968
Samuel Freundel <samuel.freundel@uni-ulm.de>
2026-06-03 18:30:44 +0200

    feature: Implementing generate_predictions_as_batch to speed up evaluation process

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from accelerate import checkpointing
 from preprocessing.cleaner import remove_prompt
 from evaluation.file_writer import write_predictions
 from evaluation.metrics_builder import compute_all_metrics
@@ -14,6 +13,7 @@ from config import (
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
+torch.set_num_threads(16)
 
 def load_model(model_path: str):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -25,16 +25,18 @@ def load_model(model_path: str):
 
     return model, tokenizer, device
 
-def generate_prediction(input_text: str, model, tokenizer, device):
+
+def generate_prediction_as_batch(input_texts: list[str], model, tokenizer, device):
     inputs = tokenizer(
-        input_text,
+        input_texts,
         return_tensors="pt",
         max_length=MAX_INPUT_LENGTH,
+        padding=True,
         truncation=True
     ).to(device)
     
     with torch.no_grad():
-        output = model.generate(
+        outputs = model.generate(
             **inputs,
             max_new_tokens=MAX_TARGET_LENGTH,
             do_sample=False,
@@ -45,22 +47,44 @@ def generate_prediction(input_text: str, model, tokenizer, device):
             encoder_no_repeat_ngram_size=0,
         )
         
-    return tokenizer.decode(output[0], skip_special_tokens=True)
+    return tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
-def generate_predictions(test_pairs, model, tokenizer, device):
+
+def generate_prediction(input_text: str, model, tokenizer, device):
+    return generate_prediction_as_batch(
+        [input_text],
+        model,
+        tokenizer,
+        device
+    )[0]
+    
+
+def generate_predictions(test_pairs, model, tokenizer, device, batch_size=16):
     candidates = []
     references = []
- 
-    for input_text, reference in test_pairs:
-        prediction = generate_prediction(input_text, model, tokenizer, device)
+
+    for i in range(0, len(test_pairs), batch_size):
+        batch = test_pairs[i:i + batch_size]
         
-        candidates.append(prediction)
-        references.append(reference)
+        input_texts = [input_text for input_text, _ in batch]
+        batch_refs = [ref for _, ref in batch]
         
+        predictions = generate_prediction_as_batch(
+            input_texts,
+            model=model,
+            tokenizer=tokenizer,
+            device=device,
+        )
+        
+        candidates.extend(predictions)
+        references.extend(batch_refs)
+    
     return candidates, references
+
 
 def extract_sources(test_pairs):
     return [remove_prompt(input_text) for input_text, _ in test_pairs]
+
 
 def evaluate_model(test_pairs, model_path: str=MODEL_OUTPUT_DIR, predictions_path: str="results.json"):
     model, tokenizer, device = load_model(model_path)
@@ -73,6 +97,7 @@ def evaluate_model(test_pairs, model_path: str=MODEL_OUTPUT_DIR, predictions_pat
     
     return compute_all_metrics(sources, candidates, references)
 
+
 def evaluate_checkpoint(test_pairs, run_model_dir: str):
     model_dir = Path(run_model_dir)
     
@@ -83,7 +108,7 @@ def evaluate_checkpoint(test_pairs, run_model_dir: str):
     
     all_results = {}
     
-    for checkpoint in checkpointing:
+    for checkpoint in checkpoints:
         print(f"Evaluating {checkpoint}")
         
         prediction_path = checkpoint / "predictions.json"
